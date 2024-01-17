b1ef5f1801c5b50a204c72950a3ce031c0a09351
DiFFtY <diffty@gmail.com>
2024-01-17 19:10:03 +0100

    String type implem with helper funcs

--- src/utils/strings.h
+++ src/utils/strings.h
@@ -4,19 +4,24 @@
 #include <string.h>
 
 #include "array.h"
+#include "memory.h"
+
 
 typedef struct String {
     char* data;
     int size;
+    int alignedSize;
     int alignment;
 } String;
 
 
-String str_create_string(int size, int aligment) {
+String str_create_string(int size, int alignment) {
     String newStr;
     newStr.size = size + 1;
-    newStr.data = (char*) malloc(newStr.size);
-    memset(newStr.data, '\0', newStr.size);
+    newStr.alignment = alignment;
+    newStr.alignedSize = calculate_size_with_padding(newStr.size, alignment);
+    newStr.data = (char*) malloc(newStr.alignedSize);
+    memset(newStr.data, '\0', newStr.alignedSize);
     return newStr;
 }
 
@@ -25,8 +30,16 @@ void str_free_string(String* s) {
 }
 
 void str_resize_string(String* s, int newSize) {
+    int newAlignedSize = calculate_size_with_padding(newSize, s->alignment);
     resize_array(&s->data, s->size, newAlignedSize);
     s->size = newSize;
+    s->alignedSize = newAlignedSize;
+}
+
+void str_set_string(String* s, char* newStr) {
+    int strSize = strlen(newStr);
+    str_resize_string(s, strSize+1);
+    memcpy(s->data, newStr, strSize+1);
 }
 
 void str_copy(char** strDst, const char* strSrc) {
--- tests/test_str.c
+++ tests/test_str.c
@@ -3,6 +3,24 @@
 
 int main() {
     String testStr = str_create_string(0, 4);
+
+    str_set_string(&testStr, "testpipi");
     printf("%s\n", testStr.data);
+    str_set_string(&testStr, "oui");
+    printf("%s\n", testStr.data);
+    str_set_string(&testStr, "CACACACACACACACA");
+    printf("%s\n", testStr.data);
+    str_set_string(&testStr, "oko1");
+    printf("%s\n", testStr.data);
+    str_set_string(&testStr, "OMG????????");
+    printf("%s\n", testStr.data);
+    str_set_string(&testStr, "");
+    printf("%s\n", testStr.data);
+    str_set_string(&testStr, "Jacques Chirac (/ʒak ʃiʁak/b Écouter), né le 29 novembre 1932 dans le 5e arrondissement de Paris et mort le 26 septembre 2019 dans le 6e arrondissement de la même ville, est un haut fonctionnaire et homme d'État français. Il est Premier ministre de 1974 à 1976, puis de 1986 à 1988, et président de la République de 1995 à 2007.");
+    printf("%s\n", testStr.data);
+    str_set_string(&testStr, "proutent^^");
+    printf("%s\n", testStr.data);
+    str_free_string(&testStr);
+
     return 0;
 }
