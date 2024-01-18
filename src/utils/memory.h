11da64b490802eae98ec466b96861c514db476cd
DiFFtY <diffty@gmail.com>
2024-01-18 16:14:25 +0100

    added print_memory_block_hex() to memory.h utils

--- src/utils/memory.h
+++ src/utils/memory.h
@@ -1,6 +1,10 @@
 #ifndef UTILS_MEMORY
 #define UTILS_MEMORY
 
+#include <stdio.h>
+#include <stdlib.h>
+
+
 int calculate_size_with_padding(int size, int alignment) {
     int paddingSize = size % alignment;
     if (paddingSize > 0) {
@@ -9,6 +13,16 @@ int calculate_size_with_padding(int size, int alignment) {
     else {
         return size;
     }
+}
+
+void print_memory_block_hex(void* pAddress, int memBlockSize) {
+    int i;
+    
+    for (i = 0; i < memBlockSize; i++) { 
+        printf("%02hhX ", ((unsigned char*) pAddress)[i]);
+    }
+    
+    printf("\n");
 }
 
 #endif
