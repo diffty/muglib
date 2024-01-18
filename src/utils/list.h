dbdcf15744b20342aa1378d828abba122704f10b
DiFFtY <diffty@gmail.com>
2024-01-18 16:16:08 +0100

    extended List functions set
    new functions : append (renamed from add), remove, insert, reallocate

--- src/utils/list.h
+++ src/utils/list.h
@@ -6,37 +6,74 @@
 
 #include "array.h"
 
+#define List(type) lst_create(sizeof(type))
+
 
 typedef struct List {
     void* data;
-    int* offsetIndex;
     int count;
     int dataSize;
     int elementSize;
 } List;
 
 
-void lst_init_list(List* pList, int elementSize) {
+void lst_init(List* pList, int elementSize) {
     pList->data = malloc(0);
     pList->count = 0;
     pList->dataSize = 0;
     pList->elementSize = elementSize;
 }
 
-void lst_add_to_list(List* pList, void* pDataToAdd) {
-    resize_array(
-        &pList->data,
-        pList->count * pList->elementSize,
-        (pList->count + 1) * pList->elementSize);
-    
-    memcpy(pList->data + pList->count * pList->elementSize,
+List lst_create(int elementSize) {
+    List newList;
+    lst_init(&newList, elementSize);
+    return newList;
+}
+
+void lst_insert(List* pList, void* pDataToAdd, int n) {
+    int oldSize = pList->count * pList->elementSize;
+    int newSize = (pList->count + 1) * pList->elementSize;
+
+    if (newSize > pList->dataSize) {
+        resize_array(&pList->data, oldSize, newSize);
+        pList->dataSize = newSize;
+    }
+
+    if (n < pList->count) {
+        memmove(pList->data + pList->elementSize * (n + 1),
+                pList->data + pList->elementSize * n,
+                pList->elementSize * (pList->count - n));
+    }
+
+    memmove(pList->data + n * pList->elementSize,
            pDataToAdd,
            pList->elementSize);
            
     pList->count++;
 }
 
-void* lst_get_element_list(List* pList, int n) {
+void lst_append(List* pList, void* pDataToAdd) {
+    lst_insert(pList, pDataToAdd, pList->count);
+}
+
+void lst_remove(List* pList, int n) {
+    if (n < pList->count - 1) {
+        memmove(pList->data + pList->elementSize * n,
+                pList->data + pList->elementSize * (n+1),
+                pList->elementSize * (pList->count - (n + 1)));
+    }
+    pList->count--;
+    memset(pList->data + pList->elementSize * pList->count, 0, pList->elementSize);
+}
+
+void lst_reallocate(List* pList) {
+    int oldSize = pList->dataSize;
+    int newSize = pList->count * pList->elementSize;
+    resize_array(&pList->data, oldSize, newSize);
+    pList->dataSize = newSize;
+}
+
+void* lst_get(List* pList, int n) {
     return (void*) pList->data + pList->elementSize * n;
 }
 
--- tests/test_list.c
+++ tests/test_list.c
@@ -2,22 +2,91 @@
 #include <stdlib.h>
 
 #include "../src/utils/list.h"
+#include "../src/utils/memory.h"
 
 
 int main() {
-    List newIntList;
-
-    lst_init_list(&newIntList, sizeof(int));
+    List newIntList = List(int);
+    
+    //lst_init_list(&newIntList, sizeof(int));
     int test1 = 5;
     int test2 = 69;
+    int test3 = 254;
+    int test4 = 5;
+    int test5 = 420;
 
-    lst_add_to_list(&newIntList, &test1);
-    int* t1 = (int*) lst_get_element_list(&newIntList, 0);
+    lst_append(&newIntList, &test1);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_append(&newIntList, &test2);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_append(&newIntList, &test3);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_remove(&newIntList, 1);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_remove(&newIntList, 0);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_append(&newIntList, &test2);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_append(&newIntList, &test3);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_append(&newIntList, &test2);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_append(&newIntList, &test1);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_append(&newIntList, &test4);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_remove(&newIntList, newIntList.count-1);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_remove(&newIntList, newIntList.count-1);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_remove(&newIntList, newIntList.count-1);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_insert(&newIntList, &test5, 1);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_insert(&newIntList, &test1, 0);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_insert(&newIntList, &test2, newIntList.count);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    lst_reallocate(&newIntList);
+    print_memory_block_hex(newIntList.data, newIntList.dataSize);
+
+    int* t1 = (int*) lst_get(&newIntList, 0);
+    int* t2 = (int*) lst_get(&newIntList, 1);
     printf("%d\n", *t1);
-
-    lst_add_to_list(&newIntList, &test2);
-    int* t2 = (int*) lst_get_element_list(&newIntList, 1);
     printf("%d\n", *t2);
+
+    List newCharPtrList = List(char*);
+
+    char testStr1[] = "cc sava";
+    char testStr2[] = "Bonjour non";
+
+    char** testPtrStr1 = &testStr1;
+    char** testPtrStr2 = &testStr2;
+
+    lst_append(&newCharPtrList, &testPtrStr1);
+    print_memory_block_hex(newCharPtrList.data, newCharPtrList.dataSize);
+
+    lst_append(&newCharPtrList, &testPtrStr2);
+    print_memory_block_hex(newCharPtrList.data, newCharPtrList.dataSize);
+
+    printf("%s\n", *((char**) lst_get(&newCharPtrList, 0)));
+    printf("%s\n", *((char**) lst_get(&newCharPtrList, 1)));
 
     return 0;
 }
